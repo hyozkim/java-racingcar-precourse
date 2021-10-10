7ae3df003561ab91f59e338ec861f3457021a889
hyozkim <sa833591@gmail.com>
2021-10-10 14:57:44 +0900

    feat: 입력 기능 개발
    - 자동차게임 입력 기능 개발
    - n 입력: 몇번의 레이싱을 하는지
    - 자동차 이름 입력 : 자동차 이름 2대 이상 ',' 구분
    입력

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,11 @@
 package racinggame;
 
+import nextstep.controller.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 자동차 경주 게임 구현
+        // 자동차경주게임 시작
+        RacingGame racingGame = new RacingGame();
+        racingGame.start();
     }
 }
